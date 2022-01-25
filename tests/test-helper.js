e4e137af52a1be0c0495b03dc2939e35f5a3087d
Oleg Larkin <oleg.larkin@nordigy.ru>
2022-01-25 10:25:46 +0300

    chore: upd to v 3.20

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
-import Application from '../app';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import config from '../config/environment';
 
 setApplication(Application.create(config.APP));
 
